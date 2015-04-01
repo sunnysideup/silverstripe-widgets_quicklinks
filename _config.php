d91f8c586c705d3c8bf1ccac22f8527f75a05ed0
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2015-04-01 11:08:47 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/widgets_quicklinks/trunk@6287 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -5,10 +5,3 @@
  * author: Nicolaas - modules [at] sunnysideup.co.nz
  **/
 
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START widgets_quicklinks MODULE ----------------===================
-//===================---------------- END widgets_quicklinks MODULE ----------------===================
-
-
-
-
